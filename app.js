339ba4be98ce736f90bd0e3af2d6bc51e318af14
Mahmoud-M-Hamdan <eng.mech.hamdan@gmail.com>
2021-08-09 17:15:41 +0300

    firstAtt

--- app.js
+++ app.js
@@ -44,7 +44,7 @@ function conImg(name, imagesrc) {
     this.name = name,
         this.image = imagesrc,
         this.shown = 0;
-        this.timeOfClick=0
+    this.timeOfClick = 0
     conImg.all.push(this);
 
 
@@ -58,17 +58,27 @@ for (let i = 0; i < imgUrl.length; i++) {
 }
 
 
-let leftImgRan 
-let centerImgRan 
-let rightImgRan 
+let leftImgRan
+let centerImgRan
+let rightImgRan
 
 
 console.log(conImg.all);
 
 function render() {
     leftImgRan = ranImg(0, imgUrl.length - 1)
-    centerImgRan = ranImg(0, imgUrl.length - 1)
-    rightImgRan = ranImg(0, imgUrl.length - 1)
+
+    let i=0;
+
+    do {
+
+        centerImgRan = ranImg(0, imgUrl.length - 1)
+        rightImgRan = ranImg(0, imgUrl.length - 1)
+
+
+    } while (leftImgRan === centerImgRan || centerImgRan === rightImgRan || leftImgRan === rightImgRan || conImg.all[i].name ===conImg.all[i+1].name );
+
+arrRan={leftImgRan,centerImgRan,rightImgRan}
 
 
 
@@ -79,31 +89,33 @@ function render() {
     leftImg.src = './img/' + conImg.all[leftImgRan].image;
     centerImg.src = './img/' + conImg.all[centerImgRan].image;
     rightImg.src = './img/' + conImg.all[rightImgRan].image;
-/*
-    let ulImg1 = document.createElement('ul')
-    lishown.appendChild(ulImg1)
-
-    let liImg1 = document.createElement('li')
-    liImg1.textContent = `${conImg.name} is appear ${conImg.all[leftImgRan].shown++}`
-    ulImg1.appendChild(liImg1)
-
-
-    let ulImg2 = document.createElement('ul')
-    lishown.appendChild(ulImg2)
-
-    let liImg2 = document.createElement('li')
-    liImg2.textContent = `${conImg.name} is appear ${conImg.all[leftImgRan].shown++}`
-    ulImg2.appendChild(liImg2)
-
-*/
-
-
-  //  conImg.all[0].name
-
-  
-   
+    /*
+        let ulImg1 = document.createElement('ul')
+        lishown.appendChild(ulImg1)
+    
+        let liImg1 = document.createElement('li')
+        liImg1.textContent = `${conImg.name} is appear ${conImg.all[leftImgRan].shown++}`
+        ulImg1.appendChild(liImg1)
+    
+    
+        let ulImg2 = document.createElement('ul')
+        lishown.appendChild(ulImg2)
+    
+        let liImg2 = document.createElement('li')
+        liImg2.textContent = `${conImg.name} is appear ${conImg.all[leftImgRan].shown++}`
+        ulImg2.appendChild(liImg2)
+    
+    */
 
 
+    //  conImg.all[0].name
+
+
+console.log(conImg.all[rightImgRan].name)
+
+console.log(conImg.all[leftImgRan].name)
+console.log(conImg.all[centerImgRan].name)
+console.log(conImg.all[i].name)
 
 
 
@@ -129,37 +141,40 @@ imgSection.addEventListener('click', clickHandler);
 function clickHandler(e) {
     if ((e.target.id === 'leftImg' || e.target.id === 'centerImg' || e.target.id === 'rightImg') && counter < numberOfRound) {
 
-if(e.target.id === 'leftImg'){
+        if (e.target.id === 'leftImg') {
 
-    conImg.all[leftImgRan].timeOfClick++;
+            conImg.all[leftImgRan].timeOfClick++;
 
-}
-if(e.target.id === 'centerImg'){
+        }
+        if (e.target.id === 'centerImg') {
 
-    conImg.all[centerImgRan].timeOfClick++;
+            conImg.all[centerImgRan].timeOfClick++;
 
-}
-if( e.target.id === 'rightImg'){
+        }
+        if (e.target.id === 'rightImg') {
 
-    conImg.all[rightImgRan].timeOfClick++;
+            conImg.all[rightImgRan].timeOfClick++;
 
-}
+        }
 
 
 
         render();
         counter++;
     }
+    else {
+        createChart();
+      }
 }
+/*
+let libutton = document.getElementById('libutton')
 
-let libutton=document.getElementById('libutton')
-
-libutton.addEventListener('click',showli)
-function showli(){
+libutton.addEventListener('click', showli)
+function showli() {
 
     for (let i = 0; i < conImg.all.length; i++) {
-        
-        
+
+
 
 
 
@@ -170,10 +185,10 @@ function showli(){
 
 
         if (conImg.all[i].shown > 0) {
-            
-            
-    
-            
+
+
+
+
 
             let liImg3 = document.createElement('li')
             liImg3.textContent = `${conImg.all[i].name} is appear ${conImg.all[i].shown} and had  ${conImg.all[i].timeOfClick} votes `
@@ -185,7 +200,7 @@ function showli(){
 
 
 }
-
+*/
 
 
 
@@ -195,3 +210,47 @@ function ranImg(min, max) {
 
 
 
+function createChart() {
+
+    let nameArr = [];
+    let shownArr = [];
+  
+    for(let i = 0; i < conImg.all.length; i++) {
+      nameArr.push(conImg.all[i].name);
+      shownArr.push(conImg.all[i].shown);
+    }
+  
+    let ctx = document.getElementById( 'myChart' ).getContext( '2d' );
+  
+    let myChart = new Chart( ctx, {
+      type: 'bar',
+      data: {
+        labels: nameArr,
+        datasets: [{
+          label: '# shown',
+          data: shownArr,
+          backgroundColor:
+                  'rgba(255, 99, 132, 0.2)',
+  
+          borderColor: [
+            'rgba(255, 99, 132, 1)',
+            'rgba(54, 162, 235, 1)',
+            'rgba(255, 206, 86, 1)',
+            'rgba(75, 192, 192, 1)',
+            'rgba(153, 102, 255, 1)',
+            'rgba(255, 159, 64, 1)'
+          ],
+          borderWidth: 2
+        }]
+      },
+      options: {
+        scales: {
+          y: {
+            beginAtZero: true
+          }
+        }
+      }
+    } );
+  }
+
+ 
